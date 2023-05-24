2ad2ce9f294484fbc9061d90622e671fa63645c2
octo <jhonatas.flor@dcx.ufpb.br>
2023-05-24 16:55:44 -0300

    dino!

--- src/dino/ModeloDino.java
+++ src/dino/ModeloDino.java
@@ -2,33 +2,100 @@ package src.dino;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
 
 public class ModeloDino extends JFrame implements KeyListener {
     private Image dinoImage;
     private int dinoX, dinoY;
     private int dinoWidth, dinoHeight;
-
+    private Timer timer;
+    private boolean isJumping;
+    private final int modifierJump = 200;
+    private final int groundPosiY = 680;
+    private final double gravity = 2.3;
+    private int contPulo = 1;
+    private Image[] dinoSprites = new Image[3];
+    BufferedImage spriteSheet;
+    
     public ModeloDino() {
-        setSize(800, 400);
+        setSize(800, 800);
         setTitle("MODELO DINO");
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        addKeyListener(this);
+        setResizable(false);
+        addKeyListener(this); 
+        
 
         // Carregar a imagem do dinossauro
-        dinoImage = Toolkit.getDefaultToolkit().getImage("src/dino/sheet_dino2.png");
-
+        //dinoImage = Toolkit.getDefaultToolkit().getImage("src/dino/sheet_dino2.png");
+        loadSprite();
+        dinoImage = dinoSprites[0];
         // Definir as coordenadas e o tamanho do recorte do dinossauro na imagem
         dinoX = 100;
-        dinoY = 300;
+        dinoY = 680;
         dinoWidth = 88;
         dinoHeight = 95;
+        isJumping = false;
+
+        // Configurar o temporizador para chamar o método paint() a cada 16 milissegundos (aproximadamente 60 FPS)
+        timer = new Timer(22, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                repaint();
+            }
+        });
+        timer.start();
     }
 
+    public void setDinoSprite(int index){
+        dinoImage = dinoSprites[index];
+    }
+    public void loadSprite(){
+        try{
+            spriteSheet = ImageIO.read(new File("src/dino/sheet_dino2.png"));
+            dinoSprites[0] = spriteSheet.getSubimage(295, 54, 88, 95);
+            dinoSprites[1] = spriteSheet.getSubimage(392, 54, 88, 95);
+            dinoSprites[2] = spriteSheet.getSubimage(104, 54, 88, 95);
+        }catch(Exception e){
+            System.out.println(e.toString());
+            
+        }
+    }
     public void paint(Graphics g) {
         super.paint(g);
+        System.out.println(this.dinoY);
         // Desenhar o recorte do dinossauro na tela
-        g.drawImage(dinoImage, dinoX, dinoY, dinoX + dinoWidth, dinoY + dinoHeight, 295, 54, 382, 148, this);
+        this.check();
+        g.drawImage(dinoImage, dinoX, dinoY, this);
+    }
+
+    public void check(){
+        if(this.isJumping){
+            setDinoSprite(2);
+            jumping();
+        }else{
+            this.down();
+        }
+    }
+
+    public void down(){
+        if(this.dinoY<680){
+            this.dinoY += 60;
+            this.isJumping = false;
+            return;
+        }
+        this.contPulo = 1;
+        setDinoSprite(0);
+    }
+
+    public void jumping(){
+        if(this.dinoY <= 280){
+            this.isJumping = false;
+            return;
+        }
+        this.dinoY -= 35;
     }
 
     public void keyTyped(KeyEvent e) {}
@@ -37,8 +104,10 @@ public class ModeloDino extends JFrame implements KeyListener {
         // Verificar se a tecla pressionada é a tecla de espaço
         if (e.getKeyCode() == KeyEvent.VK_SPACE) {
             // Fazer o dinossauro pular
-            dinoY -= 50;
-            repaint();
+            if(!this.isJumping && this.contPulo > 0){
+                this.isJumping = true;
+                this.contPulo = 0;
+            }
         }
     }
 
